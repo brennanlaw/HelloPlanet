dbd43e17ff4632fcd93117cbf9187473659c9a28
brennanlaw <32859445+brennanlaw@users.noreply.github.com>
2018-09-09 20:10:36 -0400

    Adding PlutoClass

---
+++ src/PlutoClass.java
@@ -0,0 +1,14 @@
+
+public class PlutoClass {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int firstNum = 1;
+    int secondNum = 1;
+    String message = "Values are equal";
+    if (firstNum == secondNum) {
+      System.out.println(message);
+    }
+  }
+
+}
